62e83a0d19b7f6e746920696491cf4254a7e1a5c
Alex de Andrade Soares <49697325+alexandrades@users.noreply.github.com>
2024-05-22 22:41:47 -0300

    Update db.js

--- src/model/db.js
+++ src/model/db.js
@@ -2,7 +2,7 @@ import dbConfig from "../../db.config.js";
 import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
-    dbConfig.HOST,
+    "postgres://discussit_user:6OTwWJowguT8tqMHkrCUvhjeaZiD3F11@dpg-cp79n5ed3nmc73bo2v90-a/discussit",
     {
         dialect: "postgres",
         define: {
