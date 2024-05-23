e0f5c11134bd6f537729f924d47f79e5aedeaf2a
Alex de Andrade Soares <49697325+alexandrades@users.noreply.github.com>
2024-05-22 22:34:21 -0300

    Update db.js

--- src/model/db.js
+++ src/model/db.js
@@ -2,12 +2,9 @@ import dbConfig from "../../db.config.js";
 import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
-    dbConfig.DATABASE,
-    dbConfig.USER,
-    dbConfig.PASSWORD,
+    dbConfig.HOST,
     {
-        host: dbConfig.HOST,
-        dialect: "postgresql",
+        dialect: "postgres",
         define: {
             timestamps: false
         }
